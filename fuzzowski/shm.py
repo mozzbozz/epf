18a4a75ef17e1d49e17537aadd95e32e527924f2
René Helmke <rene@0x52.net>
2020-04-27 16:13:08 +0200

    Instrumentation: changed shm.py API, removed Locking as probably not needed.
    Methods contained in Module API:
    
    * get
    * recreate
    * delete
    * reset

--- fuzzowski/shm.py
+++ fuzzowski/shm.py
@@ -1,22 +1,20 @@
 from .constants import INSTR_AFL_MAP_SIZE, INSTR_AFL_MIN_SHM_ID, INSTR_AFL_MAX_SHM_ID
 from multiprocessing import shared_memory
 import random
-from threading import Lock
 
 
 class AFLShm(shared_memory.SharedMemory):
+    """
+    AFLShm is a wrapper for multiprocessing.shared_memory.SharedMemory, which automatically initializes the
+    underlying object in such a way that it is compatible to the AFL instrumentation.
+    """
     def __init__(self, identifier: int = None):
         if identifier is None:
             identifier = str(random.SystemRandom().randint(INSTR_AFL_MIN_SHM_ID, INSTR_AFL_MAX_SHM_ID))
         super().__init__(name=identifier, create=True, size=INSTR_AFL_MAP_SIZE)
 
-    def __del__(self):
-        self.unlink()
-        self.close()
-
 
 __shm = None
-__mut = Lock()
 
 
 def get(identifier: int = None) -> AFLShm:
@@ -25,22 +23,49 @@ def get(identifier: int = None) -> AFLShm:
     Identifier is randomly drawn from /dev/urandom within (MIN_UINT32_T, MAX_UINT32_T) if is None.
 
     :param identifier: AFLShm.name
-    :return: Created shared memory object
+    :return: AFLShm
     """
-    global __mut, __shm
-    with __mut:
-        if __shm is None:
-            __shm = AFLShm(identifier=identifier)
+    global __shm
+    if __shm is None:
+        __shm = AFLShm(identifier=identifier)
+    reset()  # not sure if the SharedMemory Object properly initializes the new memory region.
     return __shm
 
 
 def reset():
-    global __mut, __shm
-    with __mut:
-        if __shm is None:
-            return get()
-        tmp = __shm
-        identifier = tmp.name
-        del tmp
-        get(identifier)
-    return __shm
+    """
+    Memset-0 the shared memory
+    """
+    global __shm
+    if __shm is None:
+        return
+    for i, _ in enumerate(__shm.buf):
+        __shm.buf[i] = 0x00
+    return
+
+
+def recreate(identifier: int = None):
+    """
+    Recreate shared memory
+
+    :param identifier: New Identifier, if none, take same identifier as last time
+    :return: AFLShm
+    """
+    global __shm
+    tmp = None
+    if __shm is not None:
+        tmp = __shm.name
+        delete()
+    return get(identifier=identifier if identifier is not None else tmp)
+
+
+def delete():
+    """
+    Delete Shared Memory
+    """
+    global __shm
+    if __shm is not None:
+        __shm.close()
+        __shm.unlink()
+    __shm = None
+
